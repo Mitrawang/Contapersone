effcc6e3f90bd64996bc2f72b85912d6d2a71bb0
Mitrawang <dimitriewang.epie@iisvittorioveneto.it>
2026-02-10 09:59:32 +0100

    Rimodifica del codice 1 (html/css/js)

--- Contapersone - Sito Web/script.js
+++ Contapersone - Sito Web/script.js
@@ -1,87 +1,21 @@
-
 const e = React.createElement;
 
-// Header
-function Header() {
-  return e(
-    "header",
-    null,
-    e("h1", null, "Il mio sito con React"),
-    e("p", null, "Senza JSX, senza build 😎")
-  );
-}
-
-// Contatore
-function Counter() {
-  const [count, setCount] = React.useState(0);
-
+function Home() {
   return e(
     "div",
-    null,
-    e("h2", null, "Contatore"),
-    e("p", null, "Valore: " + count),
+    { className: "home" },
     e(
-      "button",
-      { onClick: () => setCount(count + 1) },
-      "Aumenta"
-    )
-  );
-}
-
-// Lista
-function Lista() {
-  const [items, setItems] = React.useState(["ALALALALA"]);
-
-  function aggiungiElemento() {
-    setItems([...items, "Elemento " + (items.length + 1)]);
-  }
-
-  return e(
-    "div",
-    null,
-    e("h2", null, "Lista dinamica"),
-    e(
-      "ul",
+      "header",
       null,
-      items.map((item, index) =>
-        e("li", { key: index }, item)
-      )
-    ),
-    e(
-      "button",
-      { onClick: aggiungiElemento },
-      "Aggiungi"
+      e("h1", null, "Contapersone")
     )
   );
 }
 
-// Footer
-function Footer() {
-  return e(
-    "footer",
-    null,
-    e("p", null, "© 2026 - React CDN")
-  );
-}
-
-// App principale
 function App() {
-  return e(
-    "div",
-    null,
-    e(Header),
-    e(
-      "main",
-      null,
-      e(Counter),
-      e("hr"),
-      e(Lista)
-    ),
-    e(Footer)
-  );
+  return e(Home);
 }
 
 // Render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(e(App));
-
